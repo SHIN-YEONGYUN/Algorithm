5f6ca09667386a820a839430dccececc075f15bb
SHIN-YEONGYUN <develgyun@gmail.com>
2023-04-15 20:41:28 +0900

    알고리즘 공부

---
+++ Algo/src/algo1/algo10.java
@@ -0,0 +1,12 @@
+package algo1;
+
+import java.util.Scanner;
+
+public class algo10 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String pw = sc.next();
+		String key = sc.next();
+	}
+}
