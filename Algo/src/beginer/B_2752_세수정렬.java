59f70b05f7f2c8ff0c61a89f83fb8cb5e6e145ed
SHIN-YEONGYUN <develgyun@gmail.com>
2023-08-30 18:31:24 +0900

    추

--- Algo/src/beginer/B_2752_세수정렬.java
+++ Algo/src/beginer/B_2752_세수정렬.java
@@ -1,5 +1,20 @@
 package beginer;
 
-public class B_2752_세수정렬 {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class B_2752_세수정렬 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int[] num = new int[3];
+
+		for (int i = 0; i < 3; i++)
+			num[i] = sc.nextInt();
+		Arrays.sort(num);
+
+		for (int j : num)
+			System.out.print(j + " ");
+
+	}
 }
